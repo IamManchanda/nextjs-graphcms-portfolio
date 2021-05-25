884d6563fd4f80ef01f878bb3c03cfc7e592cd27
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-05-25 13:42:15 +0530

    Bring and Log Posts and Portfolios data to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
+import { GraphQLClient, gql } from "graphql-request";
 
-function PageIndex() {
+function PageIndex({ data }) {
+  console.log({ data });
   return (
     <>
       <Head>
@@ -11,6 +13,48 @@ function PageIndex() {
       <h1>Welcome to our awesome portfolio</h1>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const endpoint = process.env.GRAPHCMS_ENDPOINT;
+  const graphQLClient = new GraphQLClient(endpoint);
+
+  const query = gql`
+    query PostsAndPortfoliosQuery {
+      posts {
+        title
+        slug
+        description
+        date
+        id
+        tags
+        author {
+          name
+          image
+          imageWidth
+          imageHeight
+        }
+      }
+      portfolios {
+        title
+        slug
+        tags
+        description
+        date
+        coverImage
+        coverImageWidth
+        coverImageHeight
+      }
+    }
+  `;
+
+  const data = await graphQLClient.request(query);
+
+  return {
+    props: {
+      data,
+    },
+  };
 }
 
 export default PageIndex;
