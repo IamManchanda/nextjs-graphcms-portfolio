9dd50e85941bbf115d4642d61d4dd1f9dfd285eb
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-05-25 19:32:22 +0530

    Update in PortfolioItemBySlug page

--- src/pages/portfolio/[slug].tsx
+++ src/pages/portfolio/[slug].tsx
@@ -1,11 +1,21 @@
 import Head from "next/head";
+import Image from "next/image";
 import { GraphQLClient, gql } from "graphql-request";
 
 const graphQLClient = new GraphQLClient(process.env.GRAPHCMS_ENDPOINT);
 
 function PagePortfolioItemBySlug({ portfolioItem }) {
   console.log({ portfolioItem });
-  const { title } = portfolioItem;
+  const {
+    title,
+    date,
+    description,
+    tags,
+    content,
+    coverImage,
+    coverImageWidth,
+    coverImageHeight,
+  } = portfolioItem;
 
   return (
     <>
@@ -15,6 +25,21 @@ function PagePortfolioItemBySlug({ portfolioItem }) {
       </Head>
       <div className="p-10">
         <h1>{title}</h1>
+        <p>{new Date(date).toDateString()}</p>
+        <p>{description}</p>
+        <div>
+          {tags.map((tag) => (
+            <span key={tag}>{tag}</span>
+          ))}
+        </div>
+        <Image
+          alt={title}
+          title={title}
+          src={`/images/portfolio-images/${coverImage}`}
+          width={coverImageWidth}
+          height={coverImageHeight}
+          layout="responsive"
+        />
       </div>
     </>
   );
@@ -46,14 +71,13 @@ export async function getStaticProps({ params }) {
     query PortfolioItemBySlugQuery($slug: String!) {
       portfolioItem: portfolio(where: { slug: $slug }) {
         title
-        slug
-        tags
-        description
         date
+        description
+        tags
+        content
         coverImage
         coverImageWidth
         coverImageHeight
-        content
       }
     }
   `;
