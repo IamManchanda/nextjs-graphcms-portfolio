100e7e683d41777ed77380fb11097aa8764d3b78
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-05-25 12:54:43 +0530

    Install & Implement Typescript

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,13 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>Next.js GraphCMS Portfolio</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <h1>Welcome to our awesome portfolio</h1>
-    </div>
+    </>
   );
 }
