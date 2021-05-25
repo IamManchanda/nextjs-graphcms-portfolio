9f7844cd6e841c42411f64a7b353c33578472076
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-05-25 18:15:15 +0530

    Create Page: Blog Item by Slug

---
+++ src/pages/blog/[slug].tsx
@@ -0,0 +1,80 @@
+import Head from "next/head";
+import { GraphQLClient, gql } from "graphql-request";
+
+function PageBlogItemBySlug({ blogItem }) {
+  console.log({ blogItem });
+  const { title } = blogItem;
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="p-10">
+        <h1>{title}</h1>
+      </div>
+    </>
+  );
+}
+
+export async function getStaticPaths() {
+  const endpoint = process.env.GRAPHCMS_ENDPOINT;
+  const graphQLClient = new GraphQLClient(endpoint);
+
+  const query = gql`
+    query BlogItemsWithSlugQuery {
+      blogItems: posts {
+        slug
+      }
+    }
+  `;
+
+  const { blogItems } = await graphQLClient.request(query);
+
+  return {
+    paths: blogItems.map(({ slug }) => ({
+      params: { slug },
+    })),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const { slug } = params;
+
+  const endpoint = process.env.GRAPHCMS_ENDPOINT;
+  const graphQLClient = new GraphQLClient(endpoint);
+
+  const query = gql`
+    query BlogItemBySlugQuery($slug: String!) {
+      blogItem: post(where: { slug: $slug }) {
+        title
+        slug
+        description
+        date
+        id
+        tags
+        content
+        author {
+          name
+          image
+          imageWidth
+          imageHeight
+        }
+      }
+    }
+  `;
+
+  const variables = { slug };
+
+  const { blogItem } = await graphQLClient.request(query, variables);
+
+  return {
+    props: {
+      blogItem,
+    },
+  };
+}
+
+export default PageBlogItemBySlug;
