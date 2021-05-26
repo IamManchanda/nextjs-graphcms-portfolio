753ec0105100e97d120dcf1af134c14dfd6993c9
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-05-26 19:34:46 +0530

    Style update homepage - portfolio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
+import Image from "next/image";
 import { GraphQLClient, gql } from "graphql-request";
 
 const graphQLClient = new GraphQLClient(process.env.GRAPHCMS_ENDPOINT);
@@ -12,14 +13,42 @@ function PageIndex({ data }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div>
-        {data?.portfolioItems?.map(({ title, slug }) => (
-          <div key={slug}>
-            <Link href={`/portfolio/${slug}`}>
-              <a>{title}</a>
-            </Link>
-          </div>
-        ))}
+      <div className="max-w-3xl px-4 mx-auto sm:px-6 lg:px-0">
+        {data?.portfolioItems?.map(
+          ({
+            title,
+            slug,
+            description,
+            coverImage,
+            coverImageWidth,
+            coverImageHeight,
+          }) => (
+            <div key={slug}>
+              <Link href={`/portfolio/${slug}`}>
+                <a>
+                  <div className="relative mb-10">
+                    <div className="absolute z-10 w-full h-full bg-green-900 opacity-60" />
+                    <div className="absolute z-20 flex flex-col items-center justify-center w-full h-full">
+                      <h3 className="text-2xl font-semibold text-white">
+                        {title}
+                      </h3>
+                      <p className="text-lg text-gray-50">{description}</p>
+                    </div>
+                    <Image
+                      alt={title}
+                      title={title}
+                      src={`/images/portfolio-images/${coverImage}`}
+                      width={coverImageWidth}
+                      height={coverImageHeight}
+                      layout="responsive"
+                      className="absolute"
+                    />
+                  </div>
+                </a>
+              </Link>
+            </div>
+          ),
+        )}
       </div>
 
       <div className="max-w-3xl px-4 mx-auto sm:px-6 lg:px-0">
@@ -60,6 +89,10 @@ export async function getStaticProps() {
       portfolioItems: portfolios {
         title
         slug
+        description
+        coverImage
+        coverImageWidth
+        coverImageHeight
       }
       blogItems: posts {
         title
