5960c2fd8eb0c87483d3120396d3525b453fa9c3
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-05-26 21:27:07 +0530

    JumbotronHero component styling update

--- src/components/jumbotron-hero.tsx
+++ src/components/jumbotron-hero.tsx
@@ -1,6 +1,6 @@
 function JumbotronHero() {
   return (
-    <div className="flex flex-col max-w-3xl px-4 py-20 mx-auto lg:items-center sm:px-6 lg:px-0">
+    <div className="flex flex-col max-w-3xl px-4 pt-10 pb-20 mx-auto lg:items-center sm:px-6 lg:px-0">
       <h1 className="max-w-lg text-6xl font-bold text-gray-900 lg:text-center">
         The Best Portfolio <span className="text-green-700">Website</span>
       </h1>
