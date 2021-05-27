219297546e26d9f2c64081e28b894721bdd2b950
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-05-27 18:51:14 +0530

    About page updates with CMS

--- src/components/jumbotron-about.tsx
+++ src/components/jumbotron-about.tsx
@@ -1,36 +1,11 @@
-function JumbotronAbout() {
+function JumbotronAbout({ name, biography }) {
   return (
     <div className="flex flex-col max-w-3xl px-4 pt-10 pb-20 mx-auto sm:px-6 lg:px-0">
-      <h1 className="text-6xl font-bold text-gray-900">
-        <span className="text-green-700">About the Venture!</span>
+      <h1 className="text-5xl font-bold text-gray-900">
+        <span className="text-green-700">About {name}!</span>
       </h1>
       <p className="mt-8 text-xl leading-relaxed text-gray-700 sm:leading-loose">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mood alias sunt
-        debitis ab velit distinctio esse recusandae cupiditate commodi molestias
-        rerum, voluptate blanditiis! Repellat, dolores rem exercitationem
-        voluptatum cupiditate repellendus quisquam ipsam harum veniam rerum
-        totam laborum, maiores suscipit ad incidunt minus perferendis nobis
-        reprehenderit enim. Ducimus aut similique eos officia animi laudantium
-        aperiam optio maxime voluptatem, facere quos deleniti praesentium at
-        accusamus, fugit voluptate nulla. Enim dolorum, exercitationem
-        consectetur maiores ab cumque atque, placeat deserunt reprehenderit
-        neque doloremque? Cumque voluptatum minus minima distinctio aspernatur
-        sint, accusamus autem dolore odit illo commodi deserunt eum
-        necessitatibus ad vero tempore expedita beatae?
-      </p>
-      <p className="mt-8 text-xl leading-relaxed text-gray-700 sm:leading-loose">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias,
-        dignissimos odit? Ipsam, totam perspiciatis commodi ad dolores nesciunt
-        nisi atque, impedit velit numquam reiciendis! Facere hic natus possimus
-        veniam quod ducimus obcaecati. Rerum consectetur ad vero tempore animi
-        vel quis ex eveniet eum debitis illo doloribus architecto velit ratione,
-        odio nam cupiditate expedita consequuntur quae ullam tempore
-        reprehenderit? Dicta, repudiandae eaque. Nobis eaque est expedita rem
-        accusamus dolorem suscipit, voluptate ipsam, aliquam recusandae ut
-        placeat laboriosam dolor. Quia deleniti odit corrupti, eveniet hic aut
-        repellat debitis quos temporibus, perferendis repudiandae minima dolor
-        laboriosam porro velit ea quibusdam nobis tempora architecto laudantium
-        voluptas nostrum.
+        {biography}
       </p>
     </div>
   );
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,10 @@
 import Head from "next/head";
+import { GraphQLClient, gql } from "graphql-request";
 import JumbotronAbout from "../components/jumbotron-about";
 
-function PageAbout() {
+const graphQLClient = new GraphQLClient(process.env.GRAPHCMS_ENDPOINT);
+
+function PageAbout({ about }) {
   return (
     <>
       <Head>
@@ -9,9 +12,32 @@ function PageAbout() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <JumbotronAbout />
+      <JumbotronAbout name={about.name} biography={about.biography} />
     </>
   );
+}
+
+export async function getStaticProps() {
+  const query = gql`
+    query AboutpageQuery($slug: String!) {
+      about: author(where: { slug: $slug }) {
+        name
+        biography
+      }
+    }
+  `;
+
+  const variables = {
+    slug: "harry-manchanda",
+  };
+
+  const { about } = await graphQLClient.request(query, variables);
+
+  return {
+    props: {
+      about,
+    },
+  };
 }
 
 export default PageAbout;
