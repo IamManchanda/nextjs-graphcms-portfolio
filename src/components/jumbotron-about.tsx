0deb31686ddcc68695e7e85a86f820b280fe5553
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-05-27 18:12:18 +0530

    About Page updates

--- src/components/jumbotron-about.tsx
+++ src/components/jumbotron-about.tsx
@@ -1,10 +1,10 @@
 function JumbotronAbout() {
   return (
-    <div className="flex flex-col max-w-3xl px-4 pt-10 pb-20 mx-auto lg:items-center sm:px-6 lg:px-0">
-      <h1 className="max-w-lg text-6xl font-bold text-gray-900 lg:text-center">
-        <span className="text-green-700 uppercase">About</span>
+    <div className="flex flex-col max-w-3xl px-4 pt-10 pb-20 mx-auto sm:px-6 lg:px-0">
+      <h1 className="text-6xl font-bold text-gray-900">
+        <span className="text-green-700">About the Venture!</span>
       </h1>
-      <p className="mt-8 text-xl leading-loose text-gray-700">
+      <p className="mt-8 text-xl leading-relaxed text-gray-700 sm:leading-loose">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Mood alias sunt
         debitis ab velit distinctio esse recusandae cupiditate commodi molestias
         rerum, voluptate blanditiis! Repellat, dolores rem exercitationem
@@ -18,7 +18,7 @@ function JumbotronAbout() {
         sint, accusamus autem dolore odit illo commodi deserunt eum
         necessitatibus ad vero tempore expedita beatae?
       </p>
-      <p className="mt-8 text-xl leading-loose text-gray-700">
+      <p className="mt-8 text-xl leading-relaxed text-gray-700 sm:leading-loose">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias,
         dignissimos odit? Ipsam, totam perspiciatis commodi ad dolores nesciunt
         nisi atque, impedit velit numquam reiciendis! Facere hic natus possimus
